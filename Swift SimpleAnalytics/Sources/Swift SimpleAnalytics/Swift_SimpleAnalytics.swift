9f59bd057eb1d74cdc241c290580b51b7b69a383
Roel <roel@gethippo.app>
2023-10-23 15:21:44 +0200

    Adds openapi for simpleanalytics

--- Swift SimpleAnalytics/Sources/Swift SimpleAnalytics/Swift_SimpleAnalytics.swift
+++ Swift SimpleAnalytics/Sources/Swift SimpleAnalytics/Swift_SimpleAnalytics.swift
@@ -1,2 +1,14 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import OpenAPIRuntime
+import OpenAPIURLSession
+
+struct testAPI {
+    func testAPIConnection() {
+        let client = Client(
+            serverURL: try! Servers.server1(),
+            transport: URLSessionTransport()
+        )
+    }
+}
+
