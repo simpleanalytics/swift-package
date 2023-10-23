37477769905df0098c335108c6d81086ff4ccc32
Roel <roel@gethippo.app>
2023-10-23 13:12:17 +0200

    Adds openapi-generator as dependency

--- Swift SimpleAnalytics/Package.swift
+++ Swift SimpleAnalytics/Package.swift
@@ -5,17 +5,43 @@ import PackageDescription
 
 let package = Package(
     name: "Swift SimpleAnalytics",
+    platforms: [
+            .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Swift SimpleAnalytics",
             targets: ["Swift SimpleAnalytics"]),
     ],
+    dependencies: [
+            .package(url: "https://github.com/apple/swift-openapi-generator", .upToNextMinor(from: "0.3.0")),
+            .package(url: "https://github.com/apple/swift-openapi-runtime", .upToNextMinor(from: "0.3.0")),
+            .package(url: "https://github.com/apple/swift-openapi-urlsession", .upToNextMinor(from: "0.3.0")),
+        ],
+    
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Swift SimpleAnalytics"),
+            name: "Swift SimpleAnalytics",
+            dependencies: [
+                            .product(
+                                name: "OpenAPIRuntime",
+                                package: "swift-openapi-runtime"
+                            ),
+                            .product(
+                                name: "OpenAPIURLSession",
+                                package: "swift-openapi-urlsession"
+                            ),
+                        ],
+            path: "Sources",
+                        plugins: [
+                            .plugin(
+                                name: "OpenAPIGenerator",
+                                package: "swift-openapi-generator"
+                            )
+                        ]),
         .testTarget(
             name: "Swift SimpleAnalyticsTests",
             dependencies: ["Swift SimpleAnalytics"]),
