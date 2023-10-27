c2cb133c471cdbc331171696d42eebf9bbcd1c15
Roel <roel@gethippo.app>
2023-10-27 13:23:12 +0200

    Adds (hardcoded) user agent

--- SwiftSimpleAnalytics/Sources/SwiftSimpleAnalytics/SimpleAnalytics.swift
+++ SwiftSimpleAnalytics/Sources/SwiftSimpleAnalytics/SimpleAnalytics.swift
@@ -8,10 +8,12 @@
 import Foundation
 import OpenAPIRuntime
 import OpenAPIURLSession
+import WebKit
 
 public class SimpleAnalytics: NSObject {
     var hostname: String
     let client: Client
+    let userAgent: String
     
     public init(hostname: String) {
         self.hostname = hostname
@@ -19,13 +21,14 @@ public class SimpleAnalytics: NSObject {
             serverURL: try! Servers.server1(),
             transport: URLSessionTransport()
         )
+        userAgent = "Mozilla/5.0 (iPad; CPU OS 13_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"
     }
     
     public func track(view: String) async {
         do {
             let response = try await client.event(
                 body: .json(.init(
-                    _type: .pageview, hostname: hostname, event: "pageview", ua: "SwiftSimpleAnalytics", path: "view"
+                    _type: .pageview, hostname: hostname, event: "pageview", ua: userAgent, path: view
                 )))
             debugPrint(response)
             switch response {
@@ -44,7 +47,7 @@ public class SimpleAnalytics: NSObject {
         do {
             let response = try await client.event(
                 body: .json(.init(
-                    _type: .event, hostname: hostname, event: event, ua: "SwiftSimpleAnalytics"
+                    _type: .event, hostname: hostname, event: event, ua: userAgent
                 )))
             debugPrint(response)
             switch response {
--- SwiftSimpleAnalytics/Tests/Swift SimpleAnalyticsTests/Swift_SimpleAnalyticsTests.swift
+++ SwiftSimpleAnalytics/Tests/Swift SimpleAnalyticsTests/Swift_SimpleAnalyticsTests.swift
@@ -4,11 +4,16 @@ import XCTest
 final class Swift_SimpleAnalyticsTests: XCTestCase {
     func testPageview() async throws {
         let tracker = SimpleAnalytics(hostname: "simpleanalyticsswift.app")
-        await tracker.track(view: "test")
+        await tracker.track(view: "/test")
     }
     
     func testEvent() async throws {
         let tracker = SimpleAnalytics(hostname: "simpleanalyticsswift.app")
+        await tracker.track(event: "test")
+    }
+    
+    func testInvalidHostname() async throws {
+        let tracker = SimpleAnalytics(hostname: "piet.henkklaas")
         await tracker.track(event: "test")
     }
 }
