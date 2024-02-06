000d4927217fd22b2d957414b5bba5947647d865
chazchazchaz <2762386+chazchazchaz@users.noreply.github.com>
2024-02-06 02:48:38 -0800

    Update RequestDispatcher.swift to async

--- Sources/SimpleAnalytics/RequestDispatcher.swift
+++ Sources/SimpleAnalytics/RequestDispatcher.swift
@@ -5,26 +5,31 @@
 //  Created by Roel van der Kraan on 03/11/2023.
 //
 
-import Foundation
 import Alamofire
+import Foundation
 
 internal struct RequestDispatcher {
-    /// Sends the event to Simple Analytics
+    /// Sends the event to Simple Analytics asynchronously
     /// - Parameter event: the event to dispatch
-    static internal func sendEventRequest(event: Event) {
-        AF.request("https://queue.simpleanalyticscdn.com/events",
-                   method: .post,
-                   parameters: event,
-                   encoder: JSONParameterEncoder.default).responseData { response in
+    static func sendEventRequest(event: Event) async {
+        do {
+            let response = try await AF.request("https://queue.simpleanalyticscdn.com/events",
+                                                method: .post,
+                                                parameters: event,
+                                                encoder: JSONParameterEncoder.default)
+                                        .serializingData()
+                                        .response
             
-            if let httpStatusCode = response.response?.statusCode {
-                switch httpStatusCode {
-                case 201:
-                    debugPrint("Simple Analytics: Event tracked")
-                default:
-                    debugPrint("Simple Analytics: Error while tracking Event to SimpleAnalytics, response code: \(httpStatusCode)")
-                }
+            guard let httpStatusCode = response.response?.statusCode else { return }
+            
+            switch httpStatusCode {
+            case 201:
+                debugPrint("Simple Analytics: Event tracked")
+            default:
+                debugPrint("Simple Analytics: Error while tracking Event to SimpleAnalytics, response code: \(httpStatusCode)")
             }
+        } catch {
+            debugPrint("Simple Analytics: An error occurred while tracking Event - \(error.localizedDescription)")
         }
     }
 }
